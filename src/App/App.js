cf587613885a0ed7a2e98196c1a10f1cf9c3707d
Tomasz <tomasz.szawara@gmail.com>
2017-05-27 19:27:39 +0100

    implemented auth redirects and private routes

--- src/App/App.js
+++ src/App/App.js
@@ -11,7 +11,9 @@ import NavBar from './NavBar';
 import Search from './../rotues/search/Search';
 import CardsContainer from './../rotues/cards/CardsContainer';
 import MyWordsContainer from './../rotues/MyWordsContainer';
-import WordContainer from '../common/WordContainer.js';
+import WordContainer from '../common/WordContainer';
+import WordsToken from '../rotues/WordsToken';
+import Login from '../rotues/Login';
 
 const AppContainer = styled.div`
   display: flex;
@@ -34,23 +36,42 @@ const NoMatch = ({ location }) => (
   </div>
 );
 
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const logged = localStorage.getItem('wordsToken') !== null;
 
-  export default () => (
-      <Router>
-        <AppContainer>
-          <NavBar/>
-          <Scroller>
-            <Switch>
-              <Redirect exact from="/" to="/search" />
-              <Route exact path="/search" component={Search}/>
-              <Route path="/search/:wordId" render={({match}) => <Search wordId={match.params.wordId}/>}/>
-              <Route path="/myWords" component={MyWordsContainer}/>
-              <Route path="/cards" component={CardsContainer}/>
-              <Route path="/word/:wordId" render={({match}) => <WordContainer wordId={match.params.wordId}/>}/>
-              <Route component={NoMatch}/>
-            </Switch>
-          </Scroller>
-        </AppContainer>
-      </Router>
-    );
+  var renderFunction = (props) => {
+    if (logged) {
+      return <Component {...props}/>
+    }
+    else {
+      return <Redirect to={{ pathname: '/login', state: { from: props.location } }}/>
+    }
+  };
+
+  return <Route {...rest} render={renderFunction}/>;
+};
+
+export default () => {
+    // TODO:  implement navbar container and get user from /api/users/me
+  return (
+    <Router>
+      <AppContainer>
+        <NavBar/>
+        <Scroller>
+          <Switch>
+            <Redirect exact from="/" to="/search"/>
+            <Route path="/authToken/:token" render={({match}) => <WordsToken token={match.params.token}/>}/>/>
+            <Route exact path="/login" component={Login}/>
+            <PrivateRoute exact path="/search" component={Search}/>
+            <PrivateRoute path="/search/:wordId" render={({match}) => <Search wordId={match.params.wordId}/>}/>
+            <PrivateRoute path="/myWords" component={MyWordsContainer}/>
+            <PrivateRoute path="/cards" component={CardsContainer}/>
+            <PrivateRoute path="/word/:wordId" render={({match}) => <WordContainer wordId={match.params.wordId}/>}/>
+            <Route component={NoMatch}/>
+          </Switch>
+        </Scroller>
+      </AppContainer>
+    </Router>
+  )
+};
 
