9e8e4e911402a9d10f569faacd8b0bde90e995f5
Tomasz <tomasz.szawara@gmail.com>
2017-08-29 23:10:29 +0100

    moved knex migration to api

--- backend/api/API-REAMDE.md
+++ backend/api/API-REAMDE.md
@@ -8,6 +8,12 @@ API & AUTH server.
 ## In development
 * `node src/server.js --facebook-secret your-fb-secret --dev`
 
+# Production
+## SSH
+* `aws ecr get-login`
+* execute login command
+* `ssh -i ~/Desktop/tomasz-key-pair.pem ec2-user@ec2-54-89-222-246.compute-1.amazonaws.com`
+
 
 #Road-map: 
 * mask facebook-secret from log output
---
+++ backend/api/knexfile.js
@@ -0,0 +1,36 @@
+// These configs be used either by migrations or by backend itself.
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      database: 'dict',
+      user:     'root',
+      password: '123'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: 'migrations'
+    }
+  },
+
+  production: {
+    client: 'mysql',
+    connection: {
+      database: 'dict',
+      user:     'root',
+      password: '123'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: 'migrations'
+    }
+  }
+};
